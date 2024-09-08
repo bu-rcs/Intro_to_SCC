1433a15c2bafe6efdf660a4ab1a00a2bf576e2e6
Katia Bulekova <ktrn@bu.edu>
2024-09-08 13:01:04 -0400

    first version of  hello_world.py

---
+++ code/python/basic/hello_world.py
@@ -0,0 +1,13 @@
+import sys
+import pkg_resources
+
+if __name__=='__main__':
+    print("Hello World!")
+
+    # Print some info about this version of Python
+    print("\n\nPython version:\n", sys.version)
+    
+    # list installed packages
+    installed_packages = pkg_resources.working_set
+    installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages]) 
+    print(installed_packages_list)
